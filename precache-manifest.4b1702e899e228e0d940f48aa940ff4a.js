a2b05679f352df29746b7890ecf97d1eb4103588
Manjuboyz <manjuboyz@gmail.com>
2020-02-01 23:11:34 +0530

    Updates

---
+++ precache-manifest.4b1702e899e228e0d940f48aa940ff4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe457d72f77654532757bb80a5895934",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "0a0f72dfaf5152104c5f",
+    "url": "/website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef9804a867f77f9e15ac",
+    "url": "/website/static/js/2.ec1606be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/website/static/js/2.ec1606be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0f72dfaf5152104c5f",
+    "url": "/website/static/js/main.339818b8.chunk.js"
+  },
+  {
+    "revision": "29db59fd0d388444716f",
+    "url": "/website/static/js/runtime-main.b656aa90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
